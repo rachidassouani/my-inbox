db0cf60c6b6f2076a0723f868db44531ad31eed3
rachidassouani <rachidassouani@gmail.com>
2023-11-25 19:22:41 +0100

    init myinbox app

---
+++ src/main/java/io/rachidassouani/myinbox/MyinboxApplication.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.myinbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rachidassouani/myinbox/MyinboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.myinbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
